4c611479e80679148c5006c75e10cdc54b986043
Matias Morales <matias.andres.mh@gmail.com>
2020-12-23 22:45:39 -0300

    add pais services with  select option

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,31 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
-  styleUrls: ['./template.component.css']
+  styleUrls: ['./template.component.css'],
 })
 export class TemplateComponent implements OnInit {
+  usuario = {
+    nombre: 'Matías',
+    apellido: 'Morales',
+    correo: 'matias_andres@live.com',
+    pais:''
+  };
+  paises: any[] = [];
 
-usuario ={
-  nombre:'',
-  apellido:'',
-  correo:''
-}
-
-  constructor() { }
+  constructor(private paisServices: PaisService) {}
 
   ngOnInit(): void {
+    this.paisServices.getPaises().subscribe((paises) => {
+      this.paises = paises; 
+      this.paises.unshift(
+        {
+          nombre:'Selecciona un País',
+          codigo:''
+        }
+      ); // unshift para agregar un valor, en este caso el primer valor por defecto de select de paises
+ 
+    });
   }
 
-  guardar(forma: NgForm){
-
-    console.log(forma)
-    console.log(forma.value);
-    console.log('nombre:' + forma.value.nombre +
-     " Apellido: "+ forma.value.apellido +
-      " Correo: " + forma.value.correo );
+  guardar(forma: NgForm) {
+    if (forma.invalid) {
+      Object.values(forma.controls).forEach((control) => {
+        // permite recorrer los campos del formulario con el objetivo de validar los campos
+        control.markAsTouched(); // marca los campos como touched activando su validaciones
+      });
+      return;
+    }
   }
+
 
 }
